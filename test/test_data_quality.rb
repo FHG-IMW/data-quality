518b15b17613bde462bd523a8389475084508e14
Max Kießling <max.kiessling@moez.fraunhofer.de>
2013-04-05 11:31:51 +0200

    added tests

---
+++ test/test_data_quality.rb
@@ -0,0 +1,43 @@
+require 'test/unit'
+require 'test_helper'
+require 'data_quality'
+
+class TestDataQuality < Test::Unit::TestCase
+  def test_companies_should_have_6_quality_tests
+    assert_equal 6,Company.quality_tests.length
+  end
+
+  def test_company_should_respond_to_methods
+    assert Company.quality_tests
+    assert Company.first.run_quality_tests(false)
+    assert Company.first.quality_test_states
+  end
+
+  def test_run_quality_tests
+    company = Company.find_by_name("FullFeatured")
+    test_result = company.run_quality_tests
+
+    assert_equal 6, test_result.passed_tests.length
+    assert_equal 0, test_result.failed_tests.length
+    assert_equal 0, test_result.inapplicable_tests.length
+
+    assert_equal 18, test_result.quality_score
+
+
+
+
+    company = Company.last
+    test_result = company.run_quality_tests
+
+    assert_equal 3, test_result.passed_tests.length
+    assert_equal "02", test_result.passed_tests.first.identifier  # The second test should pass
+
+    assert_equal 2, test_result.failed_tests.length
+
+    assert_equal 1, test_result.inapplicable_tests.length
+    assert_equal "01", test_result.inapplicable_tests.first.identifier  # The second test should pass
+
+    assert_equal 10, test_result.quality_score
+
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,80 @@
+$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
+require 'rubygems'
+require 'sqlite3'
+require 'active_record'
+
+require 'simplecov'
+SimpleCov.start
+
+require 'data_quality'
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+
+ActiveRecord::Schema.define(:version => 1) do
+
+  create_table "quality_test_states" do |t|
+    t.string :identifier
+    t.boolean :not_applicable
+    t.integer :testable_id
+    t.string :testable_type
+
+    t.timestamps
+  end
+
+  create_table "companies", :force => true do |t|
+    t.string   :name
+    t.string   :start_of_operations
+    t.string   :end_of_operations
+    t.integer  :quality_score,                    :default => 0
+    t.integer  :failed_tests,                     :default => 0
+
+    t.timestamps
+  end
+
+  create_table "people", :force => true do |t|
+    t.string   :name
+    t.integer  :company_id
+    t.date     :employed_since
+    t.integer  :quality_score,                    :default => 0
+    t.integer  :failed_tests,                     :default => 0
+
+    t.timestamps
+  end
+end
+
+
+
+
+class Company < ActiveRecord::Base
+  has_many :people
+
+  has_quality_tests do
+    quality_test '01', :method_name => :not_empty, :attr => :name
+    quality_test '02',  :method_name => :not_empty, :attr => :start_of_operations
+    quality_test '03', :method_name => :not_empty, :attr => :end_of_operations, :if => lambda {|company| !company.start_of_operations.blank?}
+    quality_test '04', :method_name => :each_not_empty, :attr => :employed_since, :function => :people
+    quality_test '05', :description => "Company.end_of_operations is later than Company.start_of_operations", :if => lambda {|company| !company.start_of_operations.blank? and !company.end_of_operations.blank?} do |company|
+      company.start_of_operations < company.end_of_operations
+    end
+    quality_test '06', :method_name => :not_expired, :since => 1.year.ago, :attr => :updated_at
+  end
+end
+
+
+class Person < ActiveRecord::Base
+  belongs_to :company
+end
+
+full_company = Company.create(:name => "FullFeatured", :start_of_operations => "2012-01-01", :end_of_operations => "2012-02-02")
+empty_company= Company.create(:start_of_operations => "2012-03-03", :end_of_operations => "2012-02-02")
+
+
+person = Person.create(:name => "Test Fred", :employed_since => "2012-01-01")
+person.company=full_company
+person.save
+
+empty_company.quality_test_states.create(:identifier => "01", :not_applicable => true)
+empty_company.quality_test_states.create(:identifier => "02", :not_applicable => true)
+
+
+
